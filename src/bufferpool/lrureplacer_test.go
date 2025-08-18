0ea4b41541714de5e070e9fa9e6670a806688671
Danila Slesarev <blackdeer1524@yandex.ru>
2025-08-18 07:58:35 +0300

    fix

--- src/bufferpool/lrureplacer_test.go
+++ src/bufferpool/lrureplacer_test.go
@@ -4,35 +4,48 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/Blackdeer1524/GraphDB/src/pkg/common"
 )
 
 func TestLRUReplacerBasic(t *testing.T) {
 	r := NewLRUReplacer()
 
-	r.Unpin(1)
-	r.Unpin(2)
-	r.Unpin(3)
+	fileID := common.FileID(42)
+	first := common.PageIdentity{FileID: fileID, PageID: 1}
+	second := common.PageIdentity{FileID: fileID, PageID: 2}
+	third := common.PageIdentity{FileID: fileID, PageID: 3}
+	fourth := common.PageIdentity{FileID: fileID, PageID: 4}
+	fifth := common.PageIdentity{FileID: fileID, PageID: 5}
+
+	r.Unpin(first)
+	r.Unpin(second)
+	r.Unpin(third)
 
 	assert.Equal(t, uint64(3), r.GetSize())
 
-	r.Pin(2)
+	r.Pin(second)
 	assert.Equal(t, uint64(2), r.GetSize())
 
 	victim, err := r.ChooseVictim()
 	assert.NoError(t, err)
-	assert.Equal(t, uint64(1), victim)
+	assert.Equal(t, first, victim)
 
 	assert.Equal(t, uint64(1), r.GetSize())
 
-	r.Unpin(4)
-	r.Unpin(5)
+	r.Unpin(fourth)
+	r.Unpin(fifth)
 
 	assert.Equal(t, uint64(3), r.GetSize())
 
 	v1, _ := r.ChooseVictim()
 	v2, _ := r.ChooseVictim()
 
-	assert.ElementsMatch(t, []uint64{3, 4}, []uint64{v1, v2})
+	assert.ElementsMatch(
+		t,
+		[]common.PageIdentity{third, fourth},
+		[]common.PageIdentity{v1, v2},
+	)
 
 	assert.Equal(t, uint64(1), r.GetSize())
 }
