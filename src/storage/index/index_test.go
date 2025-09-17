c6929bcf14ab7d06901d4b4a37a4d7d48ad339dd
Blackdeer1524 <52544736+Blackdeer1524@users.noreply.github.com>
2025-09-17 11:08:02 +0300

    added missing begin statement (#40)

--- src/storage/index/index_test.go
+++ src/storage/index/index_test.go
@@ -881,6 +881,7 @@ func TestConcurrentIndexGrowth(t *testing.T) {
 			defer locker.Unlock(txnID)
 
 			ctxLogger := logger.WithContext(txnID)
+			require.NoError(t, ctxLogger.AppendBegin())
 			index, err := NewLinearProbingIndex(indexMeta, pool, locker, ctxLogger, true, 42)
 			if errors.Is(err, txns.ErrDeadlockPrevention) {
 				require.NoError(t, ctxLogger.AppendAbort())
